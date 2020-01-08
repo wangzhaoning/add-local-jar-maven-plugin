ef82fd527c2ef6358f15e5a2d0bb6cf9afc2bb76
Bo Zhang <zhangbodut@gmail.com>
2020-01-08 21:21:56 +0800

    Refactor some code

--- src/main/java/maven/plugin/AddLocalJar.java
+++ src/main/java/maven/plugin/AddLocalJar.java
@@ -5,9 +5,13 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,61 +26,70 @@ public class AddLocalJar extends AbstractMojo {
     @Override
     public void execute() {
         List<String> files = getFiles();
-        System.out.println("files  name is> " + files);
-        List<List<String>> newNamesResult = null;
-        try {
-            newNamesResult = processBuilderCommand(files);
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
-            assert newNamesResult != null;
-            useMvnCommand(newNamesResult.get(0), newNamesResult.get(1));
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-        printDependency(newNamesResult.get(1));
+
+        getLog().info("files  name is> " + files);
+
+        files.forEach(this::runCommandOnFile);
+
+        List<String> newNames = mapFileName(files, fileName -> getBasename(fileName) + "-1.0.jar");
+        List<String> newFilePre = mapFileName(files, this::getBasename);
+
+        useMvnCommand(newNames, newFilePre); // 说实话我还是不知道这两个参数啥意思，建议起个更有意义的名字
+        printDependency(newFilePre);
+    }
+
+    private List<String> mapFileName(List<String> originalFileNames, UnaryOperator<String> function) {
+        return originalFileNames.stream().map(function).collect(Collectors.toList());
+    }
+
+    private void runCommandOnFile(String file) {
+        run(Arrays.asList(
+                "cmd.exe", // 我不太明白为啥要cmd /c，直接rename不好么？我猜你直接fork子进程的时候碰到了问题没有解决。
+                "/c",
+                String.format("rename \"%s\" \"%s\"", file, getBasename(file) + "-1.0.jar")
+        ));
     }
 
     private void printDependency(List<String> newNamesPre) {
-        for (String newNamePre : newNamesPre) {
-
-            System.out.println(" <dependency>  \n" +
-                    "      <groupId>" + newNamePre + "</groupId>  \n" +
-                    "      <artifactId>" + newNamePre + "</artifactId>  \n" +
-                    "      <version>1.0</version>  \n" +
-                    "    </dependency>  ");
-        }
+        newNamesPre.stream().map(this::createDependency).forEach(getLog()::info);
     }
 
-    private void useMvnCommand(List<String> newNames, List<String> newNamesPre) throws IOException, InterruptedException {
+    private String createDependency(String name) {
+        return " <dependency>  \n" +
+                "      <groupId>" + name + "</groupId>  \n" +
+                "      <artifactId>" + name + "</artifactId>  \n" +
+                "      <version>1.0</version>  \n" +
+                "    </dependency>  ";
+    }
+
+    private void useMvnCommand(List<String> newNames, List<String> newNamesPre) {
         for (int i = 0; i < newNames.size(); i++) {
-            List<String> commands = new ArrayList<>();
-            commands.add("cmd.exe");
-            commands.add("/c");
-            System.out.println("mvn install:install-file -Dfile=" + newNames.get(i) + " -DgroupId=" + newNamesPre.get(i) +
-                    " -DartifactId=" + newNamesPre.get(i) + " -Dversion=1.0 -Dpackaging=jar");
-            commands.add("mvn install:install-file -Dfile=" + newNames.get(i) + " -DgroupId=" + newNamesPre.get(i) +
-                    " -DartifactId=" + newNamesPre.get(i) + " -Dversion=1.0 -Dpackaging=jar");
-            run(commands);
+            String cmd = String.format("mvn install:install-file -Dfile=%s -DgroupId=%s -DartifactId=%s -Dversion=1.0 -Dpackaging=jar",
+                    newNames.get(i), newNamesPre.get(i), newNamesPre.get(i));
+            getLog().info(cmd);
+
+            run(Arrays.asList("cmd.exe", "/c", cmd));
         }
     }
 
-    private void run(List<String> commands) throws IOException, InterruptedException {
-        ProcessBuilder pb = new ProcessBuilder(commands);
-        pb.directory(new File(path));
-        Process process = pb.start();
-        int status = process.waitFor();
-        InputStream in = process.getInputStream();
+    private void run(List<String> commands) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(commands);
+            pb.directory(new File(path));
+            Process process = pb.start();
+            process.waitFor();
+            InputStream in = process.getInputStream();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String line = br.readLine();
-        while (line != null) {
-            System.out.println(line);
-            line = br.readLine();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (Exception e) {
+            getLog().error(e);
         }
     }
-
 
     private List<String> getFiles() {
         File[] tempList = new File(path).listFiles();
@@ -85,24 +98,7 @@ public class AddLocalJar extends AbstractMojo {
         return Stream.of(tempList).filter(File::isFile).map(File::getName).collect(Collectors.toList());
     }
 
-
-    private List<List<String>> processBuilderCommand(List<String> files) throws IOException, InterruptedException {
-        List<String> newNames = new ArrayList<>();
-        List<String> newFilePre = new ArrayList<>();
-        List<List<String>> result = new ArrayList<>();
-        for (String file : files) {
-            List<String> commands = new ArrayList<>();
-            commands.add("cmd.exe");
-            commands.add("/c");
-            String[] fileName = file.split("\\.");
-            String newFileName = fileName[0] + "-1.0.jar";
-            newFilePre.add(fileName[0]);
-            commands.add("rename " + "\"" + file + "\"" + " " + "\"" + newFileName + "\"");
-            newNames.add(newFileName);
-            run(commands);
-        }
-        result.add(newNames);
-        result.add(newFilePre);
-        return result;
+    private String getBasename(String fileName) {
+        return fileName.split("\\.")[0];
     }
 }
